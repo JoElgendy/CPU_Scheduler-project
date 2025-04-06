bcf1ace7c15a1496d1d3b6720e75e8f7b9ea29e1
Adham Nasreldin <adhamnasreldin16@gmail.com>
2025-04-07 00:30:56 +0200

    Updated the process class

--- process.py
+++ process.py
@@ -1,34 +1,34 @@
+class process :  
+    """
+    This is a class encapsulating all data and attributes of a process that, 
+    we need in our CPU Process scheduler 
 
-
-class process : 
-    pid = 0  
+    We have made a class variable (incremental PID ) to auto distribute the pid on the created processes 
+    """
     incremental_pid =1  
-    remaining_time = 0
-    arrival_time = 0 
-    completion_time  = 0
-    burst_time = 0 
-    priority = 1 
 
-    def __init__(self , remaing_time , arrival_time) : 
+    def __init__(self , arrival, remaining =1 , priority =2 ) : 
+        """
+        This is the class constructor, in which we initialize all class attributes 
+        """
         self.pid = self.incremental_pid 
         self.incremental_pid += 1 
-        self.remaining_time =remaing_time 
-        self.burst_time  = remaing_time 
-        self.arrival_time = arrival_time
+        self.remaining_time =remaining
+        self.burst_time  = remaining 
+        self.arrival_time = arrival
+        self.priortiy = priority
 
-    
-    def __init__(self , remaing_time , arrival_time, priority) : 
-        self.pid = self.incremental_pid 
-        self.incremental_pid += 1 
-        self.remaining_time =remaing_time 
-        self.burst_time  = remaing_time 
-        self.arrival_time = arrival_time   
-        self.priorty= priority  
-        
-    def decrement_time(self,current): 
+    def decrement_time(self,time=1): 
+        """
+        This is used to decrement time from the remaing time of the process
+        to simulate running one timeunit on the processor 
+        """
         if ( self.remaining_time == 1 ) : 
             self.remaining_time -= 1 ; 
-            self.completion_time = current + 1 
+            self.completion_time = time + 1 
         else : 
             self.remaining_time -=1 
 
+
+p1 =process(1,0)
+help(process)
